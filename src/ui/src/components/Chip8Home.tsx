6b32c76831df29f72d3a775c842fb3869bd0f5c4
alexsousadev <alexandrosousapro@gmail.com>
2025-09-20 13:25:43 -0300

    feat: create initial CHIP-8 emulator structure with web interface - add entry point for CHIP-8 emulator with ROM loading (for load rom testing) - add CHIP-8 demo ROM files - create retro-styled home component with 90s aesthetic as first emulator screen

---
+++ src/ui/src/components/Chip8Home.tsx
@@ -0,0 +1,22 @@
+import "./Chip8Home.css"
+import { useNavigate } from "react-router"
+
+export default function Chip8Home() {
+    const navigate = useNavigate()
+    
+    return (
+       <div className="container">
+        <div className="chip8-box">
+            <h1 className="chip8-title">CHIP-8 Dive</h1>
+        </div>
+        <div className="buttons-container">
+            <button
+            className="button play-button"
+            onClick={() => navigate("/menu")}
+            >
+            Play
+            </button>
+      </div>
+       </div>
+    )
+}
